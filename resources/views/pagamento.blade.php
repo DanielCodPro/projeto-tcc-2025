f8d7a88a6c206d1e7ad1d4a91f41cbdc808b9114
kauanmmartins <134561097+kauanmmartins@users.noreply.github.com>
2025-03-27 18:20:50 -0300

    alteração no nome do arquivo de pagamento

--- resources/views/pagamento.blade.php
+++ resources/views/pagamento.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagamento</title>
+    <title>Pagamento</title> 
 </head>
 <body>
     <h1>Pagamento</h1>
